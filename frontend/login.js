9d084dcd55464f3087fb3cb46d933e7f18c2747e
babapial <pialhasansu@gmail.com>
2024-08-17 20:24:57 +0600

    first 6 class all

---
+++ frontend/login.js
@@ -0,0 +1,50 @@
+const handleLogin = async () =>{
+    //console.log("button clicked");
+    const userIdInput = document.getElementById("user-id");
+    const passwordInput = document.getElementById("password");
+
+    const userId = userIdInput.value;
+    const password = passwordInput.value;
+
+    const user = {
+        userId:userId,
+        password:password,
+    };
+    const userInfo = await fetchUserInfo(user);
+    //console.log(userInfo);
+    const errorElement = document.getElementById("user-login-error");
+    if(userInfo.length === 0) {
+        console.log("data did not found");
+        errorElement.classList.remove("hidden");
+    }
+    else{
+        errorElement.classList.add("hidden");
+        localStorage.setItem("loggedInUser", JSON.stringify(userInfo[0]));
+        window.location.href = "/frontend/post.html";
+
+    }
+
+};
+
+const fetchUserInfo = async(user) =>{
+    let data;
+   try{
+    const res = await fetch("http://localhost:1500/getUserInfo",{
+        method:"POST",
+        headers:{
+            "content-type":"application/json",
+        },
+        body:JSON.stringify(user),
+    });
+    
+    data = await res.json();
+   }
+   catch(err){
+    console.log("error connection to the server :",err);
+   }
+   finally{
+    //console.log("user info from server : ",data);
+    return data;
+
+   }
+};
---
+++ server/index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+
+const port = 1500;
+
+const app = express();
+
+//middleware
+
+app.use(cors());
+app.use(express.json());
+
+//making connection with mysql
+
+// app.get(("/example"),(req,res) =>{
+//    res.send("hellooo");
+// });
+
+
+var db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'postbook2'
+  });
+
+
+db.connect((err) =>{
+    if(err){
+        console.log("somethings went wrong connecting database : ",err);
+        throw err;
+    }
+    else {
+        console.log("mysql server connectec");
+    }
+    
+});
+
+app.post("/getUserInfo",(req,res)=>{
+    
+    const {userId,password} = req.body;
+
+    const getUserInfosql = `SELECT userId,userName,userImage FROM users WHERE users.userId =? AND userPassword = ?`;
+
+    let query = db.query(getUserInfosql,[userId,password],(err,result)=>{
+        if(err){
+            console.log("somethingg went wrong",err);
+            throw err;
+        }
+        else{
+            res.send(result);
+        }
+    } );
+})
+
+   
+ 
+
+app.listen(port, () => {
+    console.log(`server isa running on port ${port}`);
+});
